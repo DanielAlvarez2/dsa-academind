d56976ee610bcee75d1eb8bd01da34cb6a97549c
Daniel Yllanes <daniel.yllanes@hotmail.com>
2025-04-27 11:38:23 -0400

    add 2 performance.now() points to use as timers

--- functionSumUp7m45s.js
+++ functionSumUp7m45s.js
@@ -1,8 +1,11 @@
 function sumUp(n){
     let result = 0
+    const start = performance.now()
     for(let i = 1; i <= n; i++){
         result = result + i
     }
+    const end = performance.now()
+    console.log(end-start)
     return result
 }
-console.log(sumUp(4))
+console.log(sumUp(10000000000))
