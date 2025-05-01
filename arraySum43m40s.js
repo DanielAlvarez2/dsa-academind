48dbfc42b1877bcf1f7506b7c97a45076997c41c
Daniel Yllanes <daniel.yllanes@hotmail.com>
2025-05-01 13:40:56 -0400

    create a function/algorithm that calculates the sum of numbers in an array

---
+++ arraySum43m40s.js
@@ -0,0 +1,14 @@
+const arr = []
+const arraySize = Math.floor(Math.random() * 5) + 1
+for (let i = 0; i < arraySize; i++){
+    arr.push(Math.floor(Math.random() * 100) + 1)
+}
+function arraySum(arr){
+    let sum = 0
+    for (let i=0; i<arr.length;i++){
+        sum = sum + arr[i]
+    }
+    console.log(sum)
+}
+arraySum(arr)
+console.log(arr)
